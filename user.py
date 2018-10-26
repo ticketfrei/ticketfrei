7cb211b4cb7de1116c97e4afd6739ca48405f9aa
b3yond <b3yond@riseup.net>
2018-10-26 17:25:25 +0200

    polishing the wording of RSS subscription

--- user.py
+++ user.py
@@ -361,11 +361,13 @@ Aber je mehr Leute mitmachen, desto eher kannst du dir sicher
 sein, dass wir sie finden, bevor sie uns finden.
 
 Wenn du immer direkt gewarnt werden willst, kannst du auch die
-Benachrichtigungen über E-Mail, Telegram, Mastodon und RSS feed aktivieren. Gib 
-einfach <a href="/city/mail/""" + city + """"/">hier</a> deine 
-E-Mail-Adresse an, oder subscribe dem Telegram-Bot [@ticketfrei_""" + city +\
-"_bot](https://t.me/ticketfrei_" + city + """_bot), Mastodon-bot [@""" +\
-city + """](""" + masto_link + """"), oder RSS feed [""" + city + """](""" + masto_link + """.atom?replies=false&boosts=true)
+Benachrichtigungen über E-Mail, Telegram, oder den Mastodon RSS 
+feed aktivieren. Entweder:
+* Gibt hier [deine E-Mail-Adresse an](/city/mail/""" + city + """)
+* Subscribe dem Telegram-Bot [@ticketfrei_""" + city + \
+"_bot](https://t.me/ticketfrei_" + city + """_bot)
+* oder subscribe dem RSS feed von [""" + city + """](""" + masto_link + \
+""".atom?replies=false&boosts=true)
 
 Also, wenn du weniger Glück hast, und der erste bist, der einen 
 Kontrolleur sieht:
