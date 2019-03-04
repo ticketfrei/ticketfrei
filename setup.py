eabc43cdba8f5e641d8462921aa027072dd945ba
cookietime--yay <cookietime--yay@users.noreply.github.com>
2019-03-04 19:50:00 +0100

    Add setup.py file for pip module.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='ticketfrei',
+    version='',
+    packages=['ticketfrei'],
+    url='https://github.com/ticketfrei/ticketfrei',
+    license='ISC',
+    author='',
+    author_email='',
+    description='',
+    install_requires=[
+        'bottle',
+        'jwt',
+        'mastodon.py',
+        'pylibscrypt',
+        'pytoml',
+        'tweepy',
+    ],
+)
